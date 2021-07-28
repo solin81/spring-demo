5faec6a5d0cbc24cc4f40a8648b5323b122da3ca
Maciej Solinski <Maciej.Solinski@fujitsu.com>
2021-07-28 22:42:48 +0200

    Not needed

--- src/main/java/ms/app/ApplicationConfig.java
+++ src/main/java/ms/app/ApplicationConfig.java
@@ -1,10 +1,8 @@
 package ms.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 /**
  * @author Maciej Solinski <a href="mailto:maciej.solinski@ts.fujitsu.com">maciej.solinski@ts.fujitsu.com</a>
@@ -13,8 +11,5 @@ import org.springframework.core.env.Environment;
 @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = false)
 @ComponentScan("ms")
 class ApplicationConfig {
-
-    @Autowired
-    Environment environment;
 
 }
